178e935ef1d7eeba6c31bff77f2731aa59b1b60c
topcode111 <aldahir.martinez9197@gmail.com>
2021-06-04 14:28:42 +0800

    fixing settings

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
 
 if "AWS_ACCESS_KEY_ID" in os.environ and "AWS_STORAGE_BUCKET_NAME" in os.environ:
